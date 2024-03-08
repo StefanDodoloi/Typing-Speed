53bcec9e840a123a33f765987c8f5a0bce2edd8b
StefanDodoloi <153545504+StefanDodoloi@users.noreply.github.com>
2024-03-08 16:33:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 let timer, interval, randomIndex, wordLength, matchLetters;
-let seconds = 60;
+let seconds = 30;
 let correctWordsCounter = 0;
-let index = 0;
+let inputIndex = 0;
+let sentenceIndex = 0;
+let matchChars = 1;
+let inputWord = [];
+let randomSentence = [];
+let randomSentenceIndex = 0;
 
 const sentences = [
     'car table computer sea game', 'cake water house art building', 
     'tomato piece cup leaf night', 'rocket dream glass thin box', 
-    'bed the balloons flour pot', 'brick rain hole world park',
-    'cold food run city wheel', 'pen airplane lord bell date', 
+    'bed there balloons flour pot', 'brick rain hole world park',
+    'cold food run city wheel', 'pencil airplane lord bell date', 
     'ball hat tractor river fruit', 'apple ice soup hot tree', 
-    'oil chair basket ship sun', 'snow orange map moon pancake',
+    'oil chair basket ship sunflower', 'snow orange map moon pancake',
     'beach witch display road mountain', 'blow shoes battery horse banana',
     'bar paper hair planet flower', 'tower mean loaf runner untold', 
     'towel mosquito meat case bowl', 'dice nose blanket radiator parrot',
@@ -20,42 +25,55 @@ document.getElementById('timer'). innerHTML = seconds + ' s';
 document.getElementById('correctWords').innerHTML = 'Correct words: ' + correctWordsCounter;
 document.getElementById('newGameButton').addEventListener('click', startGame);
 document.getElementById('input').addEventListener('input', checkCharacters);
+document.addEventListener('keydown', (event) => {
+    if (event.key == ' ' && matchChars == 1) {
+        ++correctWordsCounter;
+        document.getElementById('correctWords').innerHTML = 'Correct words: ' + correctWordsCounter; 
+        matchChars = 1;
+    }
+});
 
 function checkCharacters() {
-    let userInput = document.getElementById('input').value; // Obținem textul introdus de utilizator
-    let randomSentence = sentences[randomIndex]; // Obținem propoziția curentă
-    let randomSentenceLength = randomSentence.length;
-    if (randomSentence[index] != ' ') {
-        ++wordLength;
-    } else {
-        wordLength = 0;
+    inputWord = (document.getElementById('input').value.trim()).split('');
+    if (randomSentence[randomSentenceIndex] === inputWord[inputIndex]) {
+        ++inputIndex;
+        ++randomSentenceIndex;
+    } else if (randomSentence[randomSentenceIndex] !== inputWord[inputIndex]) {
+        matchChars = 0;
+    } else if (randomSentence[randomSentenceIndex] === inputWord[inputIndex] && randomSentence[randomIndex] === ' ') {
+        inputIndex = randomSentenceIndex;
     }
-    if (index === randomSentenceLength - 1) {
-        wordLength = 0;
-    }
-    if (randomSentence[index] === userInput[index]) {
-        ++matchLetters;
-        // sa faci literele verzi
-    } else {
-        // sa faci literele rosii
+    console.log(inputWord[inputIndex]);
+    if (randomSentenceIndex === randomSentence.length - 1) {
+        displaySentence();
+        document.getElementById('input').value = '';
+        inputIndex = 0;
+        randomSentenceIndex = 0;
     }
 }
+
+
 
 function displaySentence() {
     randomIndex = Math.floor(Math.random() * sentences.length);
     document.getElementById('text').innerHTML = sentences[randomIndex];
+    randomSentence = sentences[randomIndex].split('');
 }
 
 function startGame() {
-    seconds = 60;
-    index = 0;
+    seconds = 30;
+    inputIndex = 0;
+    inputWord = [];
     wordLength = 0;
-    matchLetters = 0;
+    matchLetters = 1;
     correctWordsCounter = 0;
     interval = setInterval(updateTimer, 1000);
     newGameButton.style.display = 'none';
     document.getElementById('gameOverMessage').innerHTML = '';
     displaySentence();
+    document.getElementById('input').value = '';
+    document.getElementById('input').focus();
+    document.getElementById('correctWords').innerHTML = 'Correct words: ' + correctWordsCounter; 
 }
 
 function updateTimer() {
