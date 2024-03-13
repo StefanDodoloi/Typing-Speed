91cef3d6af69f73c101b48d3a31b62a8373361bf
StefanDodoloi <153545504+StefanDodoloi@users.noreply.github.com>
2024-03-13 18:41:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,94 +1,93 @@
-let timer, interval, randomIndex, wordLength, matchLetters;
-let seconds = 30;
+let intervalIdTime;
+let countSeconds = 60;
 let correctWordsCounter = 0;
 let inputIndex = 0;
-let sentenceIndex = 0;
-let matchChars = 1;
-let inputWord = [];
+let randomIndex = 0;
 let randomSentence = [];
-let randomSentenceIndex = 0;
+let textElement = document.getElementById('text');
+let inputElement = document.getElementById('input');
 
 const sentences = [
-    'car table computer sea game', 'cake water house art building', 
-    'tomato piece cup leaf night', 'rocket dream glass thin box', 
-    'bed there balloons flour pot', 'brick rain hole world park',
+    'cartoons table computer sea games', 'cake water house artist building', 
+    'tomato piece cups leaf night', 'rocket dream glass thin boxes', 
+    'butterfly flutters gently in garden', 'ocean waves crash loudly nearby',
+    'bed there balloons flour pot', 'bricks rain hole world park',
+    'moonlight whispers softly through trees', 'penguin dances gracefully on ice',
     'cold food run city wheel', 'pencil airplane lord bell date', 
-    'ball hat tractor river fruit', 'apple ice soup hot tree', 
-    'oil chair basket ship sunflower', 'snow orange map moon pancake',
+    'ball hat tractor river fruit', 'apple ice soup monkey tree', 
+    'oil chair basket baggage sunflower', 'snow orange map moon pancake',
     'beach witch display road mountain', 'blow shoes battery horse banana',
     'bar paper hair planet flower', 'tower mean loaf runner untold', 
-    'towel mosquito meat case bowl', 'dice nose blanket radiator parrot',
-    'boulder clock wrist glamorous antenna', 'vehicle hill oriented nail thick'
+    'towel mosquito meat case bowls', 'dice nose blanket radiator parrot',
+    'boulder clock wrist glamorous antenna', 'vehicle hill oriented nails thick',
+    'cats umbrella runs quickly outside', 'coffee book laughs quietly sometimes'
 ];
 
-document.getElementById('timer'). innerHTML = seconds + ' s';
+document.getElementById('timer').innerHTML = countSeconds + ' s';
 document.getElementById('correctWords').innerHTML = 'Correct words: ' + correctWordsCounter;
 document.getElementById('newGameButton').addEventListener('click', startGame);
-document.getElementById('input').addEventListener('input', checkCharacters);
-document.addEventListener('keydown', (event) => {
-    if (event.key == ' ' && matchChars == 1) {
-        ++correctWordsCounter;
-        document.getElementById('correctWords').innerHTML = 'Correct words: ' + correctWordsCounter; 
-        matchChars = 1;
-    }
-});
+inputElement.addEventListener('input', checkCharacters);
 
 function checkCharacters() {
-    inputWord = (document.getElementById('input').value.trim()).split('');
-    if (randomSentence[randomSentenceIndex] === inputWord[inputIndex]) {
+    let inputChar = inputElement.value[inputIndex];
+    let randomChar = randomSentence[randomIndex];
+    if (inputChar === randomChar) {
+        if (textElement.children[inputIndex].classList.contains('fail')) {
+            textElement.children[inputIndex].classList.remove('fail');
+        }
+        textElement.children[inputIndex].classList.add('success');
         ++inputIndex;
-        ++randomSentenceIndex;
-    } else if (randomSentence[randomSentenceIndex] !== inputWord[inputIndex]) {
-        matchChars = 0;
-    } else if (randomSentence[randomSentenceIndex] === inputWord[inputIndex] && randomSentence[randomIndex] === ' ') {
-        ++randomSentenceIndex;
-        inputIndex = randomSentenceIndex;
+        ++randomIndex;
+        if (inputChar === ' ' || randomIndex === randomSentence.length) {
+            let hasMistakes = false;
+            for (let i = 0; i < randomSentence.length; ++i) {
+                if (textElement.children[i].classList.contains('fail')) {
+                    hasMistakes = true;
+                } else {
+                    hasMistakes = false;
+                }
+            }
+            if (!hasMistakes) {
+                ++correctWordsCounter;
+                document.getElementById('correctWords').innerHTML = 'Correct words: ' + correctWordsCounter;
+            }
+        }
+    } else {
+        textElement.children[inputIndex].classList.add('fail');
     }
-    console.log(inputIndex, randomSentenceIndex);
-    if (randomSentenceIndex === randomSentence.length - 1) {
+    if (inputIndex === randomSentence.length) {
         displaySentence();
-        document.getElementById('input').value = '';
-        inputIndex = 0;
-        randomSentenceIndex = 0;
     }
 }
 
-
-
 function displaySentence() {
-    randomIndex = Math.floor(Math.random() * sentences.length);
-    document.getElementById('text').innerHTML = sentences[randomIndex];
-    randomSentence = sentences[randomIndex].split('');
+    randomSentence = sentences[Math.floor(Math.random() * sentences.length)].split('');
+    textElement.innerHTML = randomSentence.map(char => '<span>' + char + '</span>').join('');
+    inputIndex = 0;
+    randomIndex = 0;
+    inputElement.value = '';
 }
 
 function startGame() {
-    seconds = 30;
-    inputIndex = 0;
-    inputWord = [];
-    wordLength = 0;
-    matchLetters = 1;
+    countSeconds = 60;
     correctWordsCounter = 0;
-    randomSentenceIndex = 0;
-    interval = setInterval(updateTimer, 1000);
+    intervalIdTime = setInterval(updateTimer, 1000);
     newGameButton.style.display = 'none';
+    inputElement.disabled = false; 
+    inputElement.focus();
     document.getElementById('gameOverMessage').innerHTML = '';
+    document.getElementById('correctWords').innerHTML = 'Correct words: ' + correctWordsCounter;
     displaySentence();
-    document.getElementById('input').value = '';
-    document.getElementById('input').focus();
-    document.getElementById('correctWords').innerHTML = 'Correct words: ' + correctWordsCounter; 
 }
 
 function updateTimer() {
-    if (seconds > 0) {
-        --seconds;
-        displayTimer();
+    if (countSeconds > 0) {
+        --countSeconds;
+        document.getElementById('timer').innerHTML = countSeconds + ' s';
     } else {
-        clearInterval(interval);
+        clearInterval(intervalIdTime);
         document.getElementById('gameOverMessage').innerHTML = 'Game Over!';
         newGameButton.style.display = 'block';
+        inputElement.disabled = true;
     }
-}
-
-function displayTimer() {
-    document.getElementById('timer'). innerHTML = seconds + ' s';
 }
